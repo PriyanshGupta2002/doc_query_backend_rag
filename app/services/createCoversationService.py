e1d8739955a617d3e8b130271962208000ca651e
Priyansh Gupta <priyansh@Priyanshs-MacBook-Air.local>
2026-05-09 14:12:01 +0530

    fixes in requirements and create conversation service

--- app/services/createCoversationService.py
+++ app/services/createCoversationService.py
@@ -6,11 +6,10 @@ def create_conversation(db: Session, user_id: int, doc_id: int, title: str = Non
     # check if conversation already exists
     existing_conversation = db.query(Conversation).filter(
         Conversation.doc_id == doc_id, Conversation.user_id == user_id
-    )
+    ).first()
     if existing_conversation:
         return existing_conversation
     convo = Conversation(user_id=user_id, doc_id=doc_id, title=title)
-
     db.add(convo)
     db.commit()
     db.refresh(convo)
